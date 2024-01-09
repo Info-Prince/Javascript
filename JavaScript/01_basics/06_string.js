ec7cf28fa417ce4486cd55a6f0d2b42ce8b162ab
Prince Singh <145039927+Princeinfo053@users.noreply.github.com>
2024-01-09 14:29:53 +0000

    container font

--- JavaScript/01_basics/06_string.js
+++ JavaScript/01_basics/06_string.js
@@ -2,7 +2,6 @@ const name = "Prince";
 const repoCount = 50;
 
 //console.log(name + repoCount + " Value");
-
 //stringinterpulation
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
