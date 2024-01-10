76426120273d79547a2966279c58693cd2ae174e
Prince Singh <145039927+Princeinfo053@users.noreply.github.com>
2024-01-10 06:45:59 +0000

    devcontainer

---
+++ JavaScript/01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 2);
+console.log(anotherString);
+
+const newStringOne = "   prince    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://cssbyprince.netlify-app";
+console.log(url.replace("-" , "."));
+
+console.log(url.includes("prince"));
+
+console.log(gameName.split("-"));
+
