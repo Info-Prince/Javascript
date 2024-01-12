6a03ae7369220d0bc9129c90a0bba329fca30882
Prince Singh <145039927+Princeinfo053@users.noreply.github.com>
2024-01-12 08:50:38 +0000

    Arrows and IIFE in 03_basics

---
+++ JavaScript/03_basics/03_Arrows.js
@@ -0,0 +1,55 @@
+const user = {
+    username : "prince",
+    price : 999,
+
+    welcomeMessage : function () {
+        console.log(`${this.username} , welcome to website`); // this keyword current context ko refer krta hai
+        //context means kiske bare me baat ki ja rhi hai
+        // console.log(this);
+    }
+    
+}
+// user.welcomeMessage()
+// user.username = "raushan"
+// user.welcomeMessage()
+
+// console.log(this); // it's giving result as per node environment. since node environment hasn't any global object that's what we got empty object. or in browser this will refer to window global object.
+
+
+
+//chai and chai2 function are same with same result
+function chai ()    {
+    let username = "prince"
+    console.log(this.username); //it won't work here due to username is not global object of function chai. see the above example.
+}
+// chai()
+
+// const chai2 = function ()   {
+//     let username = "prince"
+//     console.log(this.username);
+// }
+// chai2()
+
+
+//////////////////// arrow function 
+const chai3 = () =>  {
+    const username = "prince"
+    console.log(this.username); 
+    console.log(this); //in this situation this will not work as well. or usually this keywork don't use in arrow function.
+}
+// chai3()
+
+////////////////Arrow function representation
+() => {} //that's it. but for performing action on it we can hold named this function and take paramenters.
+
+// const add = (num1 , num2) => {
+//     return num1+num2;
+// }
+// // add(6,10)
+// console.log(add(6,10))
+
+//implicit return 
+// const addtwo = (num1, num2) => num1 + num2; 
+// const addtwo = (num1, num2) => (num1 + num2); //both same 
+const addtwo = (num1, num2) => ({username: "hitesh"}) //here object passed
+console.log(addtwo(4,9));
---
+++ JavaScript/03_basics/04_iife.js
@@ -0,0 +1,29 @@
+// Immediately Invoked Function Expressions (IIFE)
+//global scope ke pollution ko hatane ke liye IIFE ka use kiya jata hai, sometimes global scope ke object ya variable or else function ko pollute krte hai use hi ignore krne ke liye IIFE ka use kiya jata hai.
+
+// IIFE () ()
+// (function definition) (execution call) 
+
+(function chai () {
+    console.log("DB CONNECTED");
+} ) ();
+//NOTE:- WHILE USING IIFE MUST TERMINATE IFFE SCOPE WITH (); SEMICLONE
+
+
+//using iife with arrow function
+( () => {
+        console.log("DB CONNECTED TWO");
+    } 
+) ();
+
+// (function aurcode() {
+        //named IIFE
+//     console.log("db connected");
+// }) (); 
+
+((name) => {
+    console.log(name);
+}) ("prince");
+
+
+// two types:- named and simple iife
