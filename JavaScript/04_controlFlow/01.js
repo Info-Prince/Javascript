fe407bdfbaae02ddb01a732f65a0c7d8a0f45a16
Prince Singh <145039927+Princeinfo053@users.noreply.github.com>
2024-01-14 14:28:48 +0000

    Control flow

---
+++ JavaScript/04_controlFlow/01.js
@@ -0,0 +1,25 @@
+// if 
+
+
+
+
+// <, > , <=, >=, ==, ===
+
+// if (2 == "2") {
+//     console.log("Executed");
+// }
+
+// === it's also check type of operand
+// if (2 === "2") {
+//     console.log("Executed");
+// }
+
+
+
+const isUserLoggedIn = true;
+const temperature = 44;
+if (temperature < 50) {
+    console.log("Less than 50");
+}
+else 
+    console.log("not lesser than 50");
