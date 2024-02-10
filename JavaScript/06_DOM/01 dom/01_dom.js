123bfc8e44d052f5c2ad1f7cddb8f60e1add93b2
Prince Kumar <princeinfo053@gmail.com>
2024-02-10 15:34:25 +0000

    last commit of 10 feb.

--- JavaScript/06_DOM/01 dom/01_dom.js
+++ JavaScript/06_DOM/01 dom/01_dom.js
@@ -41,6 +41,38 @@ myh1[0].style.color = 'green'
 
 //uses of forEach () function 
 const myul1 = document.querySelectorAll('li')
-myul.forEach(function (list) {
+myul1.forEach(function (list) {
     list.style.color = 'green'
 })
+
+
+//here we will convert an html collection into array, so that we could use forEach () function... let's see!
+const tempclassList = document.getElementsByClassName('list-items')
+//below code won't work
+// tempclassList.forEach(function (li) {
+//     console.log(li)
+// })
+//for make it working just convert it into array
+const convertedArray = Array.from(tempclassList) // done now you can use forEach() function
+convertedArray.forEach(function (list) {
+    list.style.color = 'orange'
+})
+
+
+
+//Work From Javascript wikipedia
+const h3 = document.querySelectorAll('.mw-headline') // class mw-headline selected though querySelectorAll
+
+h3.forEach(function (Head3) {
+    Head3.style.color = 'red';
+    Head3.style.backgroundColor = 'gray';
+    Head3.style.padding = '1rem'
+})
+// undefined
+h3.forEach(function (Head3) {
+    Head3.style.color = 'red';
+    Head3.style.backgroundColor = 'gray';
+    Head3.style.padding = '1rem';
+    Head3.innerText = 'Prince'
+})
+//here we learned how to take and manupulate values... and more concepts
