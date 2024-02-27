ca94853346192e00e9afa553d8bbb4fb6c6a755b
Prince Kumar <princeinfo053@gmail.com>
2024-02-27 06:13:05 +0000

    file arranged and started event concept in js

---
+++ JavaScript/08_Events/01_eventbasics.js
@@ -0,0 +1,9 @@
+//All events in javascript run sequentially. there  are some exception available as well but in general all events run sequentially. js sequentially run language. 
+// All these browser events will be invoked while having an activity. like:- moving cursor, clicking on button , hovering on elements etc...
+//browser events and environment event.
+
+//Have to study about :- 
+// type, timestamp, defaultPrevented
+// target, toElement, srcElement, currentTarget, 
+// clientX, clientY, screenX, screenY, 
+// altkey, crtlkey,  shiftkey, keyCode
