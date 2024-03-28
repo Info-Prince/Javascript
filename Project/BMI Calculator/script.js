3b69f82a3049d1b1da7f4bb1cc66e7aa022a2bea
Prince Kumar <princeinfo053@gmail.com>
2024-03-28 17:49:52 +0000

    BMI Calculator commit

---
+++ Project/BMI Calculator/script.js
@@ -0,0 +1,19 @@
+const calculate = document.querySelector('.submitBtn')
+const result = document.querySelector ('.result')
+
+calculate.addEventListener ('click', (e) => {
+    e.preventDefault();
+    const height = parseInt(document.querySelector ('#height').value); 
+    const weight = parseInt(document.querySelector ('#weight').value);
+
+    if( height === '' && height < 0 && isNaN(height)) {
+        result.innerHTML = `Enter Valid height Value`;
+    } else if (weight === '' && weight > 0 && isNaN(weight)) {
+        result.innerHTML = `Enter Valid Weight Value`;
+    } else {
+        const bmi = (weight / (( height * height ) / 10000)).toFixed(2);
+        console.log(bmi)
+        result.innerHTML = `${bmi}`;
+    }
+    
+})
