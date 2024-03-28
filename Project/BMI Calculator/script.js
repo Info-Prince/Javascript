713c449058404157c11327b53630080970c22ca7
Prince Kumar <princeinfo053@gmail.com>
2024-03-28 18:30:38 +0000

    final commit in project of bmi calculator

--- Project/BMI Calculator/script.js
+++ Project/BMI Calculator/script.js
@@ -14,6 +14,29 @@ calculate.addEventListener ('click', (e) => {
         const bmi = (weight / (( height * height ) / 10000)).toFixed(2);
         console.log(bmi)
         result.innerHTML = `${bmi}`;
+        compare(bmi);
     }
-    
+
 })
+
+function compare (bmi) {
+    if (bmi < 18.6) {
+        const underWeight = document.querySelector('.underWeight')
+        underWeight.innerHTML = `<p style= "padding: 0.5rem 0.7rem;
+        font-size: 1.2rem;
+        background-color: #383835;
+        ">Under Weight = Less than 18.6</p>`;
+    } else if (bmi >=18.6 && bmi < 24.9) {
+        const normalRange = document.querySelector('.normalRange')
+        normalRange.innerHTML = `<p style= "padding: 0.5rem 0.7rem;
+        font-size: 1.2rem;
+        background-color: #383835;
+        ">Normal Range = 18.6 and 24.9</p>`
+    } else if (bmi >= 24.9) {
+        const overWeight = document.querySelector('.overWeight')
+        overWeight.innerHTML = `<p style= "padding: 0.5rem 0.7rem;
+        font-size: 1.2rem;
+        background-color: #383835;
+        ">Overweight = Greater than 24.9</p>`
+    }
+}
