991fc0639e189f075d267f2cf2937d794eb82a9e
Prince Kumar <princeinfo053@gmail.com>
2024-03-28 18:08:47 +0000

    bmi calculator done

--- Project/BMI Calculator/script.js
+++ Project/BMI Calculator/script.js
@@ -6,10 +6,10 @@ calculate.addEventListener ('click', (e) => {
     const height = parseInt(document.querySelector ('#height').value); 
     const weight = parseInt(document.querySelector ('#weight').value);
 
-    if( height === '' && height < 0 && isNaN(height)) {
-        result.innerHTML = `Enter Valid height Value`;
-    } else if (weight === '' && weight > 0 && isNaN(weight)) {
-        result.innerHTML = `Enter Valid Weight Value`;
+    if( height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Incorrect Height Value ❎`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Incorrect Weight Value ❎`;
     } else {
         const bmi = (weight / (( height * height ) / 10000)).toFixed(2);
         console.log(bmi)
