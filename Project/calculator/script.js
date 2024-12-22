867aa1722810c0b777d66941d343aa7bb8311700
Prince Kumar <princeinfo053@gmail.com>
2024-12-22 15:15:56 +0000

    Calculator ready for keyboard input. now have to work on cursor input.

--- Project/calculator/script.js
+++ Project/calculator/script.js
@@ -2,14 +2,20 @@ const targetElem = document.getElementsByClassName('calculator-content');
 // console.log(targetElem);
 
 const inputVal = targetElem[0];
-// console.log(inputVal);
+let result = "";
+console.log(inputVal);
 
 document.addEventListener('keydown', (event) => {
     
-    const inputData = inputVal.value;
+    
     if(event.key === "Enter") {
-        for(let i = 0; i<inputData.length; i++) {
-            
-        }
+        let inputData = inputVal.value;
+        result = inputData;
+        result = eval(result);
+        console.log(result);
+        
+        inputVal.value = (Number(result)).toPrecision(2);
     }
+    
+
 })
