f01bc05d90c2c23d82912cd6701672ff20d99481
Prince Kumar <princeinfo053@gmail.com>
2024-12-22 16:11:58 +0000

    script added for click event too.

--- Project/calculator/script.js
+++ Project/calculator/script.js
@@ -1,21 +1,41 @@
 const targetElem = document.getElementsByClassName('calculator-content');
-// console.log(targetElem);
 
-const inputVal = targetElem[0];
+const inputVal = document.querySelector('input');
 let result = "";
-console.log(inputVal);
+// console.log(inputVal);
 
 document.addEventListener('keydown', (event) => {
-    
-    
+        
     if(event.key === "Enter") {
         let inputData = inputVal.value;
         result = inputData;
         result = eval(result);
         console.log(result);
         
-        inputVal.value = (Number(result)).toPrecision(2);
+        inputVal.value = (Number(result)).toFixed(2);
     }
-    
+})
 
+const buttons = document.querySelectorAll('.calculator-content');
+console.log(buttons);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        currelem = (event.target).innerText;
+        console.log(currelem);
+        
+        if(currelem == 'C')
+            inputVal.value = "";
+        else if (currelem === '=') {
+            let inputData = inputVal.value;
+            result = inputData;
+            result = eval(result);
+
+            inputVal.value = Number(result);
+        }
+        else {
+            inputVal.value += currelem;
+        }    
+
+    })
 })
