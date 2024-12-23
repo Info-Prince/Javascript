d70fd4aca6f032856da1225c7508e716eb23a36d
Prince Kumar <princeinfo053@gmail.com>
2024-12-23 15:26:50 +0000

    Now working calculator ready however some optimization has to be done.

--- Project/calculator/script.js
+++ Project/calculator/script.js
@@ -5,16 +5,40 @@ let result = "";
 // console.log(inputVal);
 
 document.addEventListener('keydown', (event) => {
-        
+    const operators = ['+', '-', '*', '/'];
+    
+    event.preventDefault();
+
     if(event.key === "Enter") {
         let inputData = inputVal.value;
         result = inputData;
         result = eval(result);
-        console.log(result);
         
-        inputVal.value = (Number(result)).toFixed(2);
+        if(Number.isInteger(result)) 
+        inputVal.value = Number(result);
+        else 
+        inputVal.value = (Number(result).toFixed(2))
     }
+    else if (operators.includes(event.key)){
+        let lastElem = inputVal.value.slice(-1);
+        
+        if(operators.includes(lastElem)) {
+            inputVal.value = inputVal.value.slice(0,-1) + event.key;
+        }
+        else 
+        inputVal.value += event.key;
+    }
+    else if (!isNaN(event.key) || event.key === '.') {
+        inputVal.value += event.key;
+    }
+    else if (event.key === 'Backspace') {
+        inputVal.value = inputVal.value.slice(0,-1);
+    }
+    else if (event.key === 'Escape')
+        inputVal.value = "";
 })
+
+
 
 const buttons = document.querySelectorAll('.calculator-content');
 console.log(buttons);
@@ -22,20 +46,31 @@ console.log(buttons);
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         currelem = (event.target).innerText;
-        console.log(currelem);
-        
+         
         if(currelem == 'C')
             inputVal.value = "";
         else if (currelem === '=') {
             let inputData = inputVal.value;
             result = inputData;
             result = eval(result);
-
+            
+            if(Number.isInteger(result)) 
             inputVal.value = Number(result);
+            else 
+            inputVal.value = (Number(result).toFixed(2))
         }
         else {
-            inputVal.value += currelem;
+            const operators = ['+', '-' ,'/' ,'*'];
+            let lastElem = inputVal.value.slice(-1);
+            
+            if(operators.includes(currelem) && operators.includes(lastElem)) {
+                inputVal.value = inputVal.value.slice(0, -1) + currelem;
+            } 
+            else {
+                inputVal.value += currelem;
+                console.log(inputVal.value);    
+            }
         }    
-
+        
     })
 })
